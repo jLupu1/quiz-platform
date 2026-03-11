9c2d2340fcafaf8c9741a7fd1fe0af619e7377a7
Jonathan <jlupu1@sheffield.ac.uk>
2026-03-11 15:29:49 +0000

    updated settings secret key and login/logout redirect

--- com3610/settings.py
+++ com3610/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5_xm$0dv(66cas%!kb6@v%@lg)epxj@qd($cfj$7c*v7rlzxhw'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -131,3 +131,6 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
