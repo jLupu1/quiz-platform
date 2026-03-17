62cb6314dc41e8aa431f1fe63efdda55b5b03d4b
Jonathan <jlupu1@sheffield.ac.uk>
2026-03-17 11:07:47 +0000

    side tracked to fix access to dashboards, ensure no error for anon user now goes to Log in page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,7 @@ from dashboard import views
 urlpatterns = [
     path('',views.index_dashboard,name='index'),
     path('student/',views.student_dashboard,name='student_dashboard'),
+    path('teacher/', views.teacher_dashboard, name='teacher_dashboard'),
+    path('admin/', views.admin_dashboard, name='admin_dashboard'),
+
 ]
