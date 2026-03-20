945ccbca5e3d9084c11dafc8ff41561cb8db3552
Jonathan <jlupu1@sheffield.ac.uk>
2026-03-20 14:20:26 +0000

    teacher and student course detail, bootrstrap icons in base, updated error404 page

--- courses/urls.py
+++ courses/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from courses import views
 
 urlpatterns = [
+    path('detail/<int:pk>', views.course_detail, name='course_detail'),
     path('create/',views.CourseCreateView.as_view(),name='create_course'),
     path('',views.CoursesView.as_view(),name='courses'),
+    path('detail/<int:pk>/search-students/', views.search_course_students, name='search_course_students'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,23 +1,17 @@
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.template import loader
+from django.db.models import Q
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
 
 from courses.forms import CourseCreationForm
 from courses.models import Course
+from users.models import UserRole
+from users.views import error_403
 
 
 # Create your views here.
-# @login_required(login_url='login')
-def index(request):
-    if request.user.is_authenticated:
-        return HttpResponse("Index Page")
-    else:
-        return redirect('users/login')
-
 class CoursesView(LoginRequiredMixin, ListView):
     model = Course
     context_object_name = 'courses'
@@ -42,3 +36,41 @@ class CourseCreateView(LoginRequiredMixin,UserPassesTestMixin, CreateView):
     def test_func(self):
         user = self.request.user
         return user.is_authenticated and (user.is_teacher or user.is_admin)
+
+
+@login_required(login_url='/users/login')
+def course_detail(request, pk):
+    course = get_object_or_404(Course, pk=pk)
+
+    is_admin = request.user.is_admin
+    is_enrolled = course.enrollment.filter(id=request.user.id).exists()
+
+    if not(is_admin or is_enrolled):
+        return error_403(request, exception="You are not enrolled in this module/course")
+
+    context = {
+        'course': course,
+        'default_students': course.enrollment.filter(role=UserRole.STUDENT)  # Add this!
+    }
+    # context will need course detail and quizzes
+    if request.user.is_teacher or request.user.is_admin:
+        return render(request, 'courses/course_detail_teacher.html', context)
+    else:
+        return render(request, 'courses/course_detail_student.html', context)
+
+@login_required(login_url='/users/login')
+def search_course_students(request,pk):
+    course = get_object_or_404(Course, pk=pk)
+    search_text = request.GET.get('search', '')
+
+    students = course.enrollment.filter(role=UserRole.STUDENT)
+
+    if search_text:
+        students = students.filter(
+            Q(first_name__icontains=search_text) |
+            Q(last_name__icontains=search_text) |
+            Q(username__icontains=search_text)
+        )
+
+    return render(request, 'partials/student_list_partial.html', {'students': students})
+
